88d0963850e2fc75f6412049189b1c66df3bbd15
randomlysa <su@randomlysa.com>
2017-12-08 00:00:10 -0600

    add routes, empty admin page
    home page is slideshow

--- src/App.js
+++ src/App.js
@@ -1,39 +1,20 @@
 import React, { Component } from 'react';
-import './style.css';
-import $ from 'jquery';
-import SlideshowItem from './components/SlideshowItem';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Slideshow from './components/Slideshow';
+import Admin from './components/Admin';
 
 class App extends Component {
-  componentDidMount() {
-    const fadeDuration = 0;
-    const slideDisplayDuration = '3000';
-
-    // Hide all images except first on load.
-    const displayFirstImage = setTimeout(function() {
-      if ($('#slideshow > div > div:gt(0)')) {
-          $('#slideshow > div > div:gt(0)').hide();
-          clearInterval(displayFirstImage);
-      }
-    }, 50);
-
-    // Loop through the slideshow, fading items out and in.
-    setInterval(function() {
-      $('#slideshow > div > div:first')
-        .fadeOut(fadeDuration) // fadeOut isn't working properly, set to 0 for now.
-        .next()
-        .fadeIn(fadeDuration)
-        .end()
-        .appendTo('#slideshow > div');
-    },  slideDisplayDuration);
-  } // componentDidMount
-
   render() {
     return (
-      <div id="slideshow">
-        <SlideshowItem />
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Slideshow} />
+          <Route exact path="/admin" component={Admin} />
+        </div>
+      </Router>
     )
-  }; // render
-} // class App
+  }
+} // App
 
 export default App;
---
+++ src/components/Slideshow.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import '../style.css';
+import $ from 'jquery';
+import SlideshowItem from './SlideshowItem';
+
+class Slideshow extends Component {
+  componentDidMount() {
+    const fadeDuration = 0;
+    const slideDisplayDuration = '3000';
+
+    // Hide all images except first on load.
+    const displayFirstImage = setTimeout(function() {
+      if ($('#slideshow > div > div:gt(0)')) {
+          $('#slideshow > div > div:gt(0)').hide();
+          clearInterval(displayFirstImage);
+      }
+    }, 50);
+
+    // Loop through the slideshow, fading items out and in.
+    setInterval(function() {
+      $('#slideshow > div > div:first')
+        .fadeOut(fadeDuration) // fadeOut isn't working properly, set to 0 for now.
+        .next()
+        .fadeIn(fadeDuration)
+        .end()
+        .appendTo('#slideshow > div');
+    },  slideDisplayDuration);
+  } // componentDidMount
+
+  render() {
+    return (
+      <div id="slideshow">
+        <SlideshowItem />
+      </div>
+    )
+  }; // render
+} // class App
+
+export default Slideshow;
