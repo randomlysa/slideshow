1d337d45a94d4b6b00ade9877e0a110c43def6db
randomlysa <su@randomlysa.com>
2018-01-06 16:11:57 -0600

    remove getConfigFromLocalStorage
    it is being set by persistedState in index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,4 @@
-import { loadState } from '../manageLocalStorage';
-
-export const GET_CONFIG_FROM_LOCALSTORAGE = 'GET_CONFIG_FROM_LOCALSTORAGE';
 export const UPDATE_SLIDESHOW_DURATION = 'UPDATE_SLIDESHOW_DURATION';
-
-
-export function getConfigFromLocalStorage() {
-    const request = loadState() || [];
-
-    return {
-        type: GET_CONFIG_FROM_LOCALSTORAGE,
-        payload: request
-    }
-}
-
 
 export function updateSlideshowDuration(duration) {
     return {
--- src/reducers/reducer_slideshowconfig.js
+++ src/reducers/reducer_slideshowconfig.js
@@ -1,13 +1,10 @@
 import {
-    GET_CONFIG_FROM_LOCALSTORAGE,
     UPDATE_SLIDESHOW_DURATION
 
 } from '../actions/index';
 
 export default function(state = [], action) {
     switch (action.type) {
-        case GET_CONFIG_FROM_LOCALSTORAGE:
-            return action.payload
 
         case UPDATE_SLIDESHOW_DURATION:
             return {...state, slideDuration: action.payload};
