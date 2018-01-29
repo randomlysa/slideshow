2f6071ce95c9bb185b80f90c28336350053d2e89
randomlysa <su@randomlysa.com>
2018-01-28 21:46:13 -0600

    rename file if file already exists

--- public/php/uploadFiles.php
+++ public/php/uploadFiles.php
@@ -1,8 +1,12 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-// Use later to upload file to correct slideshows folder.
-// $folder = $_POST["folder"];
+//  Function to generate a short random string based off the filename.
+function randomizeFilename($filename) {
+    $getFilename = explode(".", $filename);
+    $random =  md5(uniqid($getFilename[0], true));
+    return $getFilename[0] . '_' . $random . "." . $getFilename[1];
+}
 
 // Adapted from davidwalsh.name/basic-file-uploading-php
 if($_FILES['photo']['name']) {
@@ -19,10 +23,19 @@ if($_FILES['photo']['name']) {
 		if($_FILES['photo']['size'] > (3024000)) {
             print "Your file is too large.";
             exit;
-		}
+        }
+
+        // Check if filename already exists.
+        $path = '../slideshows/' . $_POST['folder'] . '/';
+        $file =  $_FILES['photo']['name'];
+        $newFile = $file;
+        while (file_exists($path . $newFile)) {
+            print "Renaming file. $newFile\n";
+            $newFile = randomizeFilename($file);
+        }
 
         // Move uploaded file to proper directory.
-		move_uploaded_file($_FILES['photo']['tmp_name'], '../slideshows/' . $_POST['folder'] . '/' . $_FILES['photo']['name']);
+		move_uploaded_file($_FILES['photo']['tmp_name'], $path . $newFile);
 		print "File uploaded.";
 
     // Other error.
