016ae10a5fa1eebbb9aeaa7c9cced1116b0bfdd4
randomlysa <su@randomlysa.com>
2018-02-03 10:34:41 -0600

    fix indents (4 to 2 spaces)

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,26 +4,24 @@ import { bindActionCreators } from 'redux';
 import { fetchWeatherFromOpenWeather } from '../actions/weather';
 
 class Weather extends Component {
-
-    componentWillMount() {
+  componentWillMount() {
         this.props.fetchWeatherFromOpenWeather();
-    }
+  }
 
-    render() {
-        return (
-            <h1 className="weather">{this.props.weather.main.temp}&deg; F</h1>
-        )
-    }
+  render() {
+    return (
+      <h1 className="weather">{this.props.weather.main.temp}&deg; F</h1>
+    )
+  }
 }
 
 function mapStateToProps({ weather }) {
-    return { weather } ;
-  }
+  return { weather } ;
+}
 
-  function mapDispatchToProps(dispatch) {
-    // Assign all actions (import * as actionCreators) to props.actions
-    return bindActionCreators( {fetchWeatherFromOpenWeather }, dispatch);
+function mapDispatchToProps(dispatch) {
+  // Assign all actions (import * as actionCreators) to props.actions
+  return bindActionCreators( {fetchWeatherFromOpenWeather }, dispatch);
+}
 
-  }
-
-  export default connect(mapStateToProps, mapDispatchToProps)(Weather);
+export default connect(mapStateToProps, mapDispatchToProps)(Weather);
