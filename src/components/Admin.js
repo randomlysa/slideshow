b655d434ec5a7c48ebc6d673ace4a8cdd740cb88
randomlysa <su@randomlysa.com>
2018-02-04 15:36:10 -0600

    missing semicolon

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { checkPassword, logout } from '../actions/admin'
+import { checkPassword, logout } from '../actions/admin';
 
 import $ from 'jquery';
 import { API_ROOT } from '../api-config';
