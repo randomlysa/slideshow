b0007817479d55b39f24341dbd6f6f969daeb08a
randomlysa <su@randomlysa.com>
2018-03-10 23:10:07 -0600

    write/update loadedCsv to database
    now handling multiple files

--- src/actions/actions_csv.js
+++ src/actions/actions_csv.js
@@ -1,15 +1,81 @@
 import Papa from 'papaparse';
 import { API_ROOT, SLIDESHOW_ROOT } from '../config/api-config';
 import $ from 'jquery';
+import _ from 'lodash';
 
 export const GET_CSV_DATA = 'GET_CSV_DATA';
 export const GET_CSV_DATA_FULFILLED = 'GET_CSV_DATA_FULFILLED';
+
+
+ // On success, save this CSV file
+ const saveLoadedCsvToDatabase = function(slideshowDir, fileObject, fileObjectFromDB) {
+  const url = `${API_ROOT}/php/sqliteUpdateDatabaseConfig.php`;
+  let newCsvForDatabase;
+
+  // Nothing in the database. Add fileObject to the database.
+  if (!fileObjectFromDB) {
+    // Make an array of one file in case new files are added later.
+    const makeArray = [fileObject];
+    $.ajax({
+      url,
+      type: 'POST',
+      data: {
+        name: slideshowDir,
+        loadedCsv: JSON.stringify(makeArray)
+      }
+    })
+    .done(() => {
+      console.log('!fileObjectFromDB - saved loadedCsv to database');
+    });
+  } else {
+    // Search for fileObject.filename and fileObject.md5 in database.
+    const findFileInDb = _.find(fileObjectFromDB, function(o) {
+      return o.filename === fileObject.filename;
+    });
+
+    const findMd5InDb = _.find(fileObjectFromDB, function(o) {
+      return o.md5 === fileObject.md5;
+    });
+
+    const { filename, md5 } = fileObject;
+    const fromDBFilename = fileObjectFromDB.filename;
+    const fromDBmd5 = fileObjectFromDB.md5;
+
+    if (findFileInDb && findMd5InDb) {
+      console.log('~~~~~~~~~everything is the same');
+    } else if (findFileInDb && !findMd5InDb) {
+      console.log('~~~~~~~Filename is the same but md5 is new');
+    } else if (findFileInDb === undefined) {
+      console.log('~~~~~~~~~~Filename is not in the database');
+      // Get existing {database info} and add the new object.
+      newCsvForDatabase = [...fileObjectFromDB, fileObject];
+    } else {
+      console.log('~~~~~~~~~~~something something')
+    }
+  } // else
+
+  // If there is new information to add, do it here.
+  if(newCsvForDatabase) {
+    $.ajax({
+      url,
+      type: 'POST',
+      data: {
+        name: slideshowDir,
+        loadedCsv: JSON.stringify(newCsvForDatabase)
+      }
+    })
+    .done(() => {
+      console.log('saved loadedCsv to database - newCsvForDatabase');
+    })
+  }
+};
+
 
 export function getCSVData(fileObject, slideshowDir) {
   const itemUrl = `${SLIDESHOW_ROOT}/slideshows/${slideshowDir}/${filename}`;
   const { filename, md5 } = fileObject;
 
-  let loadedCsvFromDatabase, fromDBFilename, fromDBmd5;
+  let loadedCsvFromDatabase, fileObjectFromDB, fromDBFilename, fromDBmd5;
   // Get loaded CSV from database.
   $.ajax({
     url: `${API_ROOT}/php/sqliteGetBulletinConfigByName.php?name=${slideshowDir}`,
@@ -20,29 +86,12 @@ export function getCSVData(fileObject, slideshowDir) {
     if (data.loadedCsv === "") {
       console.log('no data');
     } else {
-      const jsonData = JSON.parse(data.loadedCsv)
-      fromDBFilename = jsonData.filename;
-      fromDBmd5 = jsonData.md5;
-      console.log(fromDBFilename, fromDBmd5);
+      fileObjectFromDB = JSON.parse(data.loadedCsv);
+      console.log(fileObjectFromDB)
+      fromDBFilename = fileObjectFromDB.filename;
+      fromDBmd5 = fileObjectFromDB.md5;
     }
   });
-
-  // On success, save this CSV file
-  const saveLoadedCsvToDatabase = function() {
-    const url = `${API_ROOT}/php/sqliteUpdateDatabaseConfig.php`;
-    $.ajax({
-      url,
-      type: 'POST',
-      data: {
-        name: slideshowDir,
-        loadedCsv: JSON.stringify(fileObject)
-      }
-    })
-    .done(() => {
-      console.log('saved loadedCsv to database');
-    })
-  };
-
 
   return {
       type: GET_CSV_DATA,
@@ -52,7 +101,9 @@ export function getCSVData(fileObject, slideshowDir) {
             download: true,
             complete: function (response){
               try {
-                saveLoadedCsvToDatabase(),
+                // I'm counting on fileObjectFromDB to resolve faster than
+                // Papa.parse - this could be a problem?
+                saveLoadedCsvToDatabase(slideshowDir, fileObject, fileObjectFromDB),
                 fulfill({
                   filename: filename,
                   data: response.data
