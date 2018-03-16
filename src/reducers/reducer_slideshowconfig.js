8fddeb24b8e22c40d05a11742b551f38b61da1af
randomlysa <su@randomlysa.com>
2018-03-16 15:19:07 -0500

    when no config is found in the database
    return empty strings

--- src/reducers/reducer_slideshowconfig.js
+++ src/reducers/reducer_slideshowconfig.js
@@ -9,6 +9,20 @@ import {
 export default function(state = {}, action) {
     switch (action.type) {
         case GET_CONFIG_FROM_DATABASE_FULFILLED:
+
+            // If nothing has been configured in the database for this
+            // slideshow, action.payload will be undefined.
+            if (action.payload === undefined) {
+                return {
+                    slideDuration: '',
+                    transitionDuration: '',
+                    slidesToShowWeatherOn: '',
+                    cityToShowWeatherFor: '',
+                    loadedCsv: '',
+                    slideOrder: ''
+                };
+            }
+
             const {
                 slideDuration,
                 transitionDuration,
