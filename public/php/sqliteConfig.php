96cbc0e9e7005a82b654c1cc24bb9a1a5717d29d
randomlysa <su@randomlysa.com>
2018-03-15 23:57:11 -0500

    move sqlite file out of project folder
    was causing unwanted refreshes when the database was updated (written
    to)

--- public/php/sqliteConfig.php
+++ public/php/sqliteConfig.php
@@ -3,7 +3,8 @@
 
 	// http://theonlytutorials.com/php-pdo-sqlite-example-insert-read-search/
 	/* Create / Connection to sqlite using PDO and set error mode */
-		$db = new PDO('sqlite:bulletin.sqlite3');
+		define('DB_PATH', $_SERVER['DOCUMENT_ROOT'] . '/bulletin.sqlite3');
+		$db = new PDO('sqlite:' . DB_PATH);
 	/* end */
 
 	/*** if you want to use mysql database then uncomment the below lines and comment the above lines ***/
