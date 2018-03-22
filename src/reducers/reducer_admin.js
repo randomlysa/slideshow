d2d1155d6651487cc89067156ec89078e0d68ca6
randomlysa <su@randomlysa.com>
2018-03-22 00:06:02 -0500

    finished todo

--- src/reducers/reducer_admin.js
+++ src/reducers/reducer_admin.js
@@ -3,9 +3,6 @@ import {
 } from '../actions/actions_admin';
 
 export default function(state = {}, action) {
-
-    // Todo: when a slideshow is running, this doesn't get 'saved' - whatever
-    // the current value is, that's what gets returned.
     switch (action.type) {
         case DO_LOGIN:
             return {
