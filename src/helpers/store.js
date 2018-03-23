fccb3aed89ce670b4bac97e19d9399fe31d93c7e
randomlysa <su@randomlysa.com>
2018-03-22 23:19:57 -0500

    move store to own file

---
+++ src/helpers/store.js
@@ -0,0 +1,30 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import crossTabMiddleware from 'cross-tab-middleware';
+
+import slideshowApp from '../reducers';
+
+// https://github.com/rt2zz/redux-persist
+const persistConfig = {
+  key: 'slideshow',
+  storage
+};
+
+// (config, reducer)
+const persistedReducer = persistReducer(persistConfig, slideshowApp);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export let store = createStore(
+  persistedReducer,
+  composeEnhancers(
+  applyMiddleware(
+      promiseMiddleware(),
+      crossTabMiddleware('slideshow')
+  ))
+);
+
+export let persistor = persistStore(store);
+persistStore(store);
--- src/index.js
+++ src/index.js
@@ -1,39 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import promiseMiddleware from 'redux-promise-middleware';
-import thunk from 'redux-thunk';
-import { persistStore, persistReducer } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
-import storage from 'redux-persist/lib/storage';
-import crossTabMiddleware from 'cross-tab-middleware';
 
-import './index.css';
-import slideshowApp from './reducers';
+import { persistor, store } from './helpers/store.js';
 import MyRoutes from './routers/MyRoutes';
-
-// https://github.com/rt2zz/redux-persist
-const persistConfig = {
-    key: 'slideshow',
-    storage
-};
-
-// (config, reducer)
-const persistedReducer = persistReducer(persistConfig, slideshowApp);
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-let store = createStore(
-    persistedReducer,
-    composeEnhancers(
-    applyMiddleware(
-        promiseMiddleware(),
-        crossTabMiddleware('slideshow')
-    ))
-);
-
-let persistor = persistStore(store);
-persistStore(store);
+import './index.css';
 
 ReactDOM.render(
     <Provider store={store}>
