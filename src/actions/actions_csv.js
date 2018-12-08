177985fee7065ff2f1d4d017eb7f5e79b772da62
randomlysa <su@randomlysa.com>
2018-12-07 18:59:44 -0600

    add action updateCSVData

--- src/actions/actions_csv.js
+++ src/actions/actions_csv.js
@@ -4,7 +4,9 @@ import $ from 'jquery';
 import _ from 'lodash';
 
 export const GET_CSV_DATA = 'GET_CSV_DATA';
+export const UPDATE_CSV_DATA = 'UPDATE_CSV_DATA';
 export const GET_CSV_DATA_FULFILLED = 'GET_CSV_DATA_FULFILLED';
+export const UPDATE_CSV_DATA_FULFILLED = 'UPDATE_CSV_DATA_FULFILLED';
 
 export function getCSVData(fileObject, slideshowDir) {
   const { filename, md5 } = fileObject;
@@ -12,6 +14,37 @@ export function getCSVData(fileObject, slideshowDir) {
 
   return {
     type: GET_CSV_DATA,
+    async payload() {
+      return new Promise(function(fulfill, reject) {
+        Papa.parse(
+          itemUrl,
+          {
+            download: true,
+            complete: function(response) {
+              try {
+                fulfill({
+                  filename,
+                  md5,
+                  data: response.data
+                });
+              } catch (ex) {
+                reject(ex);
+              }
+            } // complete
+          },
+          reject
+        ); // Papa.parse
+      }); // return new Promise
+    } // async payload
+  }; // return
+} // export function
+
+export function updateCSVData(fileObject, slideshowDir) {
+  const { filename, md5 } = fileObject;
+  const itemUrl = `${SLIDESHOW_ROOT}/slideshows/${slideshowDir}/${filename}`;
+
+  return {
+    type: UPDATE_CSV_DATA,
     async payload() {
       return new Promise(function(fulfill, reject) {
         Papa.parse(
