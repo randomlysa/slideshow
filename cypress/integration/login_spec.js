26743d4dc90ea4a96e00bf6b9fe1e401a4cc2a1b
randomlysa <su@randomlysa.com>
2018-12-11 05:01:38 -0600

    update login tests

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,32 +1,30 @@
-import '../support/admin.js';
-import { gzipSync } from 'zlib';
-
-// Before each, do a login, since I can't do anything without a login.
 describe('The admin section', function() {
   it('should redirect from /admin to /login when not signed in', function() {
-    cy.visit('http://localhost:3000/admin')
-    cy.location('pathname').should('eq', '/login')
-  })
+    cy.visit('http://localhost:3000/admin/test');
+    cy.location('pathname').should('eq', '/login');
+  });
 
   it('should fail when using incorrect username / password', function() {
+    cy.visit('http://localhost:3000/admin/test');
     cy.get('[data-cy=usernameLogin]')
-    .type('abc')
-    .get('[data-cy=passwordLogin]')
-    .type('def')
-    .get('[data-cy=loginForm]').submit()
+      .type('abc')
+      .get('[data-cy=passwordLogin]')
+      .type('def')
+      .get('[data-cy=loginForm]')
+      .submit();
     // seems my login doesn't throw an error so expect the page to still be
     // /login
-    cy.location('pathname').should('eq', '/login')
-  })
+    cy.location('pathname').should('eq', '/login');
+  });
 
   it('should let me log in', function() {
-    cy.visit('http://localhost:3000/login')
-    .get('[data-cy=usernameLogin]')
-    .type('sasha')
-    .get('[data-cy=passwordLogin]')
-    .type('sasha')
-    .get('[data-cy=loginForm]').submit()
-    cy.location('pathname').should('eq', '/admin')
-  })
-})
-
+    cy.visit('http://localhost:3000/login/test')
+      .get('[data-cy=usernameLogin]')
+      .type('test')
+      .get('[data-cy=passwordLogin]')
+      .type('test')
+      .get('[data-cy=loginForm]')
+      .submit();
+    cy.location('pathname').should('eq', '/admin');
+  });
+});
